d494581f5bb86db5ee88a8556201e23a337eafa6
eltonq15 <eltonq15@gmail.com>
2024-09-10 10:56:13 +0100

    whitelist cors domains

--- el-creatas-server/index.js
+++ el-creatas-server/index.js
@@ -34,6 +34,24 @@ const {
 } = require("./service/user");
 const app = express();
 
+const whitelist = [
+  process.env.LOCAL_ENV,
+  process.env.TEST_ENV,
+  process.env.PROD_ENV,
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 app.use(cors());
 app.use(express.json());
 
