67b4379a8faa6b4b5d9c92b31d2b5c1e8486be14
eltonq15 <eltonq15@gmail.com>
2024-09-16 13:02:05 +0100

    isolate ui from server

--- el-creatas-ui/src/api/products-api.ts
+++
@@ -1,6 +0,0 @@
-//sera usado com useQuery
-export const getAllProducts: () => Promise<any> = () => {
-  return fetch(
-    `${process.env.REACT_APP_API_URL}/products`
-  ).then((res) => res.json());
-};
---
+++ src/api/products-api.ts
@@ -0,0 +1,6 @@
+//sera usado com useQuery
+export const getAllProducts: () => Promise<any> = () => {
+  return fetch(`${process.env.REACT_APP_API_URL}/products`).then((res) =>
+    res.json()
+  );
+};
