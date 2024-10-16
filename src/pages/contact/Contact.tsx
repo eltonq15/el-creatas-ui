7cd1cf2c9795209f79c6e10275a294ff5f1e9559
Elton Alves Ribeiro <eltonq15@gmail.com>
2024-10-16 18:08:57 +0100

    Create email sending, and confirmation message

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -6,14 +6,17 @@ import {
   Textarea,
   Typography,
 } from "@mui/joy";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { SolidButton } from "../../components/button/SolidButton";
+import { MailBoxIcon } from "../../assets/icons/MailBoxIcon";
 
 import "./styles.scss";
 
 export const Contact = () => {
+  const [messageSent, setMessageSent] = useState(false);
   const contactFormSchema = z.object({
     name: z.string().min(2, { message: "Nome inválido" }),
     email: z.string().email({ message: "Email inválido" }),
@@ -29,6 +32,29 @@ export const Contact = () => {
   } = useForm<ContactForm>({
     resolver: zodResolver(contactFormSchema),
   });
+
+  const onSubmit = async (data: ContactForm) => {
+    try {
+      const response = await fetch("http://localhost:1234/api/send-email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        alert("Email enviado com sucesso!");
+        setMessageSent(true);
+      } else {
+        const errorData = await response.json();
+        alert(`Falha no envio do email: ${errorData.message}`);
+      }
+    } catch (error) {
+      console.error("Erro ao enviar o email:", error);
+      alert("Erro ao enviar o email.");
+    }
+  };
 
   return (
     <Stack
@@ -90,88 +116,120 @@ export const Contact = () => {
         Para um atendimento mais rápido, preencha o formulário abaixo e nossa
         equipa entrará em contacto com você em breve.
       </Typography>
-      <form
-        className="contact-form"
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit(() => {
-            alert("Enviado");
-          });
-        }}
-        style={{ width: "80%" }}
-      >
-        <Stack sx={{ gap: "1rem", width: "100%", display: "flex", flex: 1 }}>
-          <Stack
-            direction={"row"}
+      {messageSent ? (
+        <Stack
+          sx={{
+            gap: "1rem",
+            width: "80%",
+            height: 300,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-evenly",
+            border: "dashed",
+          }}
+        >
+          <MailBoxIcon />
+          <Typography
+            level="h2"
             sx={{
-              gap: "1rem",
-              display: "flex",
-              width: "100%",
-              flexWrap: "wrap",
+              textAlign: "center",
+              fontSize: "24px",
+              lineHeight: "36px",
+              fontFamily: "math, serif",
+              fontWeight: "600",
+              width: "80%",
+              justifyContent: "center",
+              alignItems: "center",
             }}
           >
-            <FormControl sx={{ flexGrow: 1 }}>
-              <Input
-                {...register("name")}
-                placeholder="Nome"
-                onChange={(e) => {
-                  e.target.focus();
-                }}
-                sx={{
-                  backgroundColor: "transparent",
-                  border: "0.3px solid gray",
-                  borderRadius: 0,
-                  padding: 1.5,
-                }}
-              />
-              {errors.name && (
-                <FormHelperText>{errors.name.message}</FormHelperText>
-              )}
-            </FormControl>
-            <FormControl sx={{ flexGrow: 1 }}>
-              <Input
-                {...register("email")}
-                placeholder="Email"
-                onChange={(e) => {
-                  e.target.focus();
-                }}
-                sx={{
-                  backgroundColor: "transparent",
-                  border: "0.3px solid gray",
-                  borderRadius: 0,
-                  padding: 1.5,
-                }}
-              />
-              {errors.email && (
-                <FormHelperText>{errors.email.message}</FormHelperText>
-              )}
-            </FormControl>
-          </Stack>
-          <FormControl>
-            <Textarea
-              {...register("message")}
-              placeholder="Mensagem"
-              onChange={(e) => {
-                e.target.focus();
-              }}
-              sx={{
-                backgroundColor: "transparent",
-                border: "0.3px solid gray",
-                borderRadius: 0,
-                padding: 1.5,
-                height: 200,
-                "--Input-focusedHighlight": "none",
-              }}
-            />
-            {errors.message && (
-              <FormHelperText>{errors.message.message}</FormHelperText>
-            )}
-          </FormControl>
-          <SolidButton type="submit" disabled={isSubmitting}>
-            Enviar
-          </SolidButton>
+            Agrademos pela sua mensagem. Responderemos em breve.
+          </Typography>
         </Stack>
-      </form>
+      ) : (
+        <form
+          className="contact-form"
+          onSubmit={handleSubmit(onSubmit)}
+          style={{ width: "80%" }}
+        >
+          <Stack sx={{ gap: "1rem", width: "100%", display: "flex", flex: 1 }}>
+            <Stack
+              direction={"row"}
+              sx={{
+                gap: "1rem",
+                display: "flex",
+                width: "100%",
+                flexWrap: "wrap",
+              }}
+            >
+              <FormControl sx={{ flexGrow: 1 }}>
+                <Input
+                  {...register("name")}
+                  placeholder="Nome"
+                  onChange={(e) => {
+                    e.target.focus();
+                  }}
+                  sx={{
+                    backgroundColor: "transparent",
+                    border: "0.3px solid gray",
+                    borderRadius: 0,
+                    padding: 1.5,
+                  }}
+                />
+                {errors.name && (
+                  <FormHelperText sx={{ color: "red" }}>
+                    {errors.name.message}
+                  </FormHelperText>
+                )}
+              </FormControl>
+              <FormControl sx={{ flexGrow: 1 }}>
+                <Input
+                  {...register("email")}
+                  placeholder="Email"
+                  onChange={(e) => {
+                    e.target.focus();
+                  }}
+                  sx={{
+                    backgroundColor: "transparent",
+                    border: "0.3px solid gray",
+                    borderRadius: 0,
+                    padding: 1.5,
+                  }}
+                />
+                {errors.email && (
+                  <FormHelperText sx={{ color: "red" }}>
+                    {errors.email.message}
+                  </FormHelperText>
+                )}
+              </FormControl>
+            </Stack>
+            <FormControl>
+              <Textarea
+                {...register("message")}
+                placeholder="Mensagem"
+                onChange={(e) => {
+                  e.target.focus();
+                }}
+                sx={{
+                  backgroundColor: "transparent",
+                  border: "0.3px solid gray",
+                  borderRadius: 0,
+                  padding: 1.5,
+                  height: 200,
+                  "--Input-focusedHighlight": "none",
+                }}
+              />
+              {errors.message && (
+                <FormHelperText sx={{ color: "red" }}>
+                  {errors.message.message}
+                </FormHelperText>
+              )}
+            </FormControl>
+            <SolidButton type="submit" disabled={isSubmitting}>
+              Enviar
+            </SolidButton>
+          </Stack>
+        </form>
+      )}
     </Stack>
   );
 };
